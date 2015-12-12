89be20a7f05fc43d156320ffa581786d211fdfcf
BoldKey <adam.spanel@seznam.cz>
2015-12-12 10:36:22 +0100

    collisions work better now, there is still a rare bug when bouncing off 2 walls at the same time

--- game/player.js
+++ game/player.js
@@ -29,11 +29,11 @@ function Player() {
 		var mod = (this.speedx > 0 ? 16 : 0);
 		var h = (this.speedx > 0 ? 1 : -1);
 		if (Math.floor((this.x + this.speedx + mod + 0.01 * h) / 24) === Math.floor((this.x + mod) / 24 + h)) {
-			for (var i = 0; i < ((this.y % 24 < 8) ? 1 : 2); ++i) {
+			for (var i = 0; i < ((this.y % 24 < 8 || (this.y + this.speedy) % 24 < 8) ? 1 : 2); ++i) {
 				var tile = level.code[y + i][x + h];
 				if (tile === 1 && c) {
-					if (math.abs(speedx) <  0.03) {
-						speedx = sign(speedx) * 0.03
+					if (Math.abs(this.speedx) <  0.5) {
+						this.speedx = Math.sign(this.speedx) * 0.5;
 					}
 					this.speedx = - this.speedx;
 					c = false;
@@ -43,15 +43,17 @@ function Player() {
 				}
 			}
 		}
-		var c = true;
-		var mod = (this.speedy > 0 ? 16 : 0);
-		var h = (this.speedy > 0 ? 1 : -1);
+		x = Math.floor(this.x / 24)
+		y = Math.floor(this.y / 24)
+		c = true;
+		mod = (this.speedy > 0 ? 16 : 0);
+		h = (this.speedy > 0 ? 1 : -1);
 		if (Math.floor((this.y + this.speedy + mod + 0.01 * h) / 24) === Math.floor((this.y + mod) / 24 + h)) {
 			for (var i = 0; i < ((this.x % 24 < 8) ? 1 : 2); ++i) {
 				var tile = level.code[y + h][x + i];
 				if (tile === 1 && c) {
-					if (math.abs(speedy) <  0.03) {
-						speedx = sign(speedy) * 0.03
+					if (Math.abs(this.speedy) < 0.5) {
+						this.speedy = Math.sign(this.speedy) * 0.5;
 					}
 					this.speedy = - this.speedy;
 					c = false;
