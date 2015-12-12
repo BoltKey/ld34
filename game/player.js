725108e66388dae10a5f83ace8ad6c1ca36cad11
BoldKey <adam.spanel@seznam.cz>
2015-12-12 09:44:52 +0100

    Adjusted level and added little condition to checkcols

--- game/player.js
+++ game/player.js
@@ -25,26 +25,30 @@ function Player() {
 	this.checkcollision = function() {
 		var x = Math.floor(this.x / 24)
 		var y = Math.floor(this.y / 24)
+		var c = true;
 		var mod = (this.speedx > 0 ? 16 : 0);
 		var h = (this.speedx > 0 ? 1 : -1);
 		if (Math.floor((this.x + this.speedx + mod) / 24) === Math.floor((this.x + mod) / 24 + h)) {
 			for (var i = 0; i < ((this.y % 24 < 8) ? 1 : 2); ++i) {
 				var tile = level.code[y + i][x + h];
-				if (tile === 1) {
+				if (tile === 1 && c) {
 					this.speedx = - this.speedx;
+					c = false;
 				}
 				if (tile === 2) {
 					this.die();
 				}
 			}
 		}
+		var c = true;
 		var mod = (this.speedy > 0 ? 16 : 0);
 		var h = (this.speedy > 0 ? 1 : -1);
 		if (Math.floor((this.y + this.speedy + mod) / 24) === Math.floor((this.y + mod) / 24 + h)) {
 			for (var i = 0; i < ((this.x % 24 < 8) ? 1 : 2); ++i) {
 				var tile = level.code[y + h][x + i];
-				if (tile === 1) {
+				if (tile === 1 && c) {
 					this.speedy = - this.speedy;
+					c = false;
 				}
 				if (tile === 2) {
 					this.die();
