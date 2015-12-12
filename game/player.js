8b956ba3aee5a4a54016444e193e67006721e5aa
BoldKey <adam.spanel@seznam.cz>
2015-12-12 09:59:29 +0100

    tweak to collisions in case of very low speed

--- game/player.js
+++ game/player.js
@@ -28,10 +28,13 @@ function Player() {
 		var c = true;
 		var mod = (this.speedx > 0 ? 16 : 0);
 		var h = (this.speedx > 0 ? 1 : -1);
-		if (Math.floor((this.x + this.speedx + mod) / 24) === Math.floor((this.x + mod) / 24 + h)) {
+		if (Math.floor((this.x + this.speedx + mod + 0.01 * h) / 24) === Math.floor((this.x + mod) / 24 + h)) {
 			for (var i = 0; i < ((this.y % 24 < 8) ? 1 : 2); ++i) {
 				var tile = level.code[y + i][x + h];
 				if (tile === 1 && c) {
+					if (math.abs(speedx) <  0.03) {
+						speedx = sign(speedx) * 0.03
+					}
 					this.speedx = - this.speedx;
 					c = false;
 				}
@@ -43,10 +46,13 @@ function Player() {
 		var c = true;
 		var mod = (this.speedy > 0 ? 16 : 0);
 		var h = (this.speedy > 0 ? 1 : -1);
-		if (Math.floor((this.y + this.speedy + mod) / 24) === Math.floor((this.y + mod) / 24 + h)) {
+		if (Math.floor((this.y + this.speedy + mod + 0.01 * h) / 24) === Math.floor((this.y + mod) / 24 + h)) {
 			for (var i = 0; i < ((this.x % 24 < 8) ? 1 : 2); ++i) {
 				var tile = level.code[y + h][x + i];
 				if (tile === 1 && c) {
+					if (math.abs(speedy) <  0.03) {
+						speedx = sign(speedy) * 0.03
+					}
 					this.speedy = - this.speedy;
 					c = false;
 				}
