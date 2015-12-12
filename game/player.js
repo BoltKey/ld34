a10e0fb854881e7be7e00f3aca45538ea055dc7a
BoldKey <adam.spanel@seznam.cz>
2015-12-12 09:38:30 +0100

    can't believe this horrible hack worked. Bouncing off right and left walls.

--- game/player.js
+++ game/player.js
@@ -25,9 +25,11 @@ function Player() {
 	this.checkcollision = function() {
 		var x = Math.floor(this.x / 24)
 		var y = Math.floor(this.y / 24)
-		if (Math.floor((this.x + this.speedx + 16) / 24) === Math.floor((this.x + 16) / 24 + 1)) {
+		var mod = (this.speedx > 0 ? 16 : 0);
+		var h = (this.speedx > 0 ? 1 : -1);
+		if (Math.floor((this.x + this.speedx + mod) / 24) === Math.floor((this.x + mod) / 24 + h)) {
 			for (var i = 0; i < ((this.y % 24 < 8) ? 1 : 2); ++i) {
-				var tile = level.code[y + i][x + 1];
+				var tile = level.code[y + i][x + h];
 				if (tile === 1) {
 					this.speedx = - this.speedx;
 				}
